c07436f589cb5b44699875d3fed9c9cbb4117afb
Numan Haspolat <haspolatnuman@gmail.com>
2024-02-10 20:44:53 +0300

    upgrade

--- app.js
+++ app.js
@@ -25,47 +25,55 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 document.addEventListener("DOMContentLoaded", function () {
-    const avatar = document.querySelector(".avatar");
-    const mobileNav = document.querySelector(".hidden.mobile");
-  
-    function toggleMobileNav() {
-      gsap.to(mobileNav, {
-        y: mobileNav.style.display === "none" ? 0 : -50,
-        opacity: mobileNav.style.display === "none" ? 1 : 0,
-        duration: 0.5,
-        display: mobileNav.style.display === "none" ? "flex" : "none",
-      });
-    }
-  
-    if (window.innerWidth <= 680) {
-      avatar.addEventListener("click", toggleMobileNav);
-    }
+  const avatar = document.querySelector(".avatar");
+  const mobileNav = document.querySelector(".hidden.mobile");
+
+  function toggleMobileNav() {
+    gsap.to(mobileNav, {
+      y: mobileNav.style.display === "none" ? 0 : -50,
+      opacity: mobileNav.style.display === "none" ? 1 : 0,
+      duration: 0.5,
+      display: mobileNav.style.display === "none" ? "flex" : "none",
+    });
+  }
+
+  if (window.innerWidth <= 680) {
+    avatar.addEventListener("click", toggleMobileNav);
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var contactLinks = document.querySelectorAll(".contact-link");
+  var featuresSection = document.querySelector(".features");
+  var footer = document.querySelector("footer");
+
+  contactLinks.forEach(function (link) {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+      if (link.classList.contains("mbl")) {
+        footer.scrollIntoView({ behavior: "smooth" });
+      } else {
+        featuresSection.scrollIntoView({ behavior: "smooth" });
+      }
+    });
   });
-  
-  document.addEventListener('DOMContentLoaded', function() {
-    var contactLinks = document.querySelectorAll('.contact-link');
-    var featuresSection = document.querySelector('.features');
-    var footer = document.querySelector('footer');
-
-    contactLinks.forEach(function(link) {
-        link.addEventListener('click', function(event) {
-            event.preventDefault();
-            if (link.classList.contains('mbl')) {
-                footer.scrollIntoView({ behavior: 'smooth' });
-            } else {
-                featuresSection.scrollIntoView({ behavior: 'smooth' });
-            }
-        });
-    });
 });
-document.addEventListener('DOMContentLoaded', function() {
-    var aboutMeLink = document.querySelector('.about');
-  
-    aboutMeLink.addEventListener('click', function(event) {
-        event.preventDefault();
-        window.location.href = 'aboutme.html';
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  var aboutMeLink = document.querySelector(".about");
+
+  aboutMeLink.addEventListener("click", function (event) {
+    event.preventDefault();
+    window.location.href = "aboutme.html";
+  });
 });
 
-
-
+document.addEventListener("DOMContentLoaded", function () {
+  const yearSpan = document.querySelector(".this-year");
+  const myAge = document.querySelector(".my-age");
+  if (yearSpan) {
+    yearSpan.textContent = new Date().getFullYear();
+  }
+  if (myAge) {
+    myAge.textContent = new Date().getFullYear() - 2007;
+  }
+});
